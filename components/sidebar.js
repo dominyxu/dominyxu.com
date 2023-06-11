b54a0a4a0badee49dfce770d87cbf5e160fc3cf3
dominyxu <dominyxu@outlook.com>
2023-06-11 03:47:53 -0400

    sidebar updares

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,15 +1,25 @@
 import Link from 'next/link';
-import styles from './sidebar.module.css'
+import sidebarStyles from './sidebar.module.css'
+import Image from 'next/image';
+import utilStyles from '../styles/utils.module.css';
 
-export default function sideBar() {
+
+export default function SideBar() {
     return (
-        <nav className='nav'>
-            <ul>
+        <div>
+            <nav className={sidebarStyles.sidebarcontainer}>
+                <Link href="/"><Image
+                    src="/images/h2s_profile.jpeg"
+                    className={utilStyles.borderCircle}
+                    height={144}
+                    width={144}
+                    alt=""
+                /></Link>
                 <Link href="/about">about</Link>
                 <Link href="/experience">experience</Link>
                 <Link href="/projects">projects</Link>
                 <Link href="/interests">interests</Link>
-            </ul>
-        </nav>
+            </nav>
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,26 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import SideBar from '../components/sidebar';
 
 export default function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>i'm a 3rd year computer engineering student @ mcmasteru, seeking an  internship.
-          I previously developed software for Trigon, implemented cloud automation features at the Ontario Ministry of Health.
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
+    <div>
+      <SideBar></SideBar>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>i'm a 3rd year computer engineering student @ mcmasteru, seeking an  internship.
+            I previously developed software for Trigon, implemented cloud automation features at the Ontario Ministry of Health.
+          </p>
+          <p>
+            (This is a sample website - you’ll be building a site like this on{' '}
+            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+      </Layout>
+    </div>
   );
 }
