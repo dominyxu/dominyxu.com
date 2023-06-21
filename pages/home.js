630a8f070fbda04f083ca5bc82667edfae04f170
dominyxu <dominyxu@outlook.com>
2023-06-21 15:43:06 -0400

    icons

--- pages/home.js
+++ pages/home.js
@@ -1,29 +1,56 @@
 import Link from "next/link";
+import Head from "next/head";
+import { useEffect } from "react";
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <script src="https://kit.fontawesome.com/c1d5331308.js" crossorigin="anonymous"></script>
-            </Head>
-            <div className='main'>
-                <div className='bio'>
-                    <h1>Hey! I’m Dominy</h1>
-                    <br></br>
-                    <p>
-                        I am a third year computer engineering student based in Toronto studying @McMasterU. I’m interested in cloud technologies, machine learning and fintech with the ultimate goal of creating better products through software.
-                        <br></br>
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://kit.fontawesome.com/c1d5331308.js";
+    script.crossOrigin = "anonymous";
+    script.async = true;
+    document.body.appendChild(script);
 
-                        Feel free to reach out to me at <a href='mailto:xudominy32@gmail.com'>xudominy32@gmail.com</a> and take a look at my resume!
-                    </p>
-                </div>
-                <div className='socials'>
-                    <ul>
-                        <li><Link href='https://github.com/dominyxu'><i className="fa-brands fa-square-github">hi</i></Link></li>
-                    </ul>
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div className="main">
+        <div className="bio">
+          <h1>Hey! I’m Dominy</h1>
+          <br></br>
+          <p>
+            I am a third year computer engineering student based in Toronto
+            studying @McMasterU. I’m interested in cloud technologies, machine
+            learning and fintech with the ultimate goal of creating better
+            products through software.
+            <br></br>
+            Feel free to reach out to me at{" "}
+            <a href="mailto:xudominy32@gmail.com">xudominy32@gmail.com</a> and
+            take a look at my resume!
+          </p>
+        </div>
+        <br></br>
+        <div className="socials">
+          <ul>
+            <li>
+              <Link href="https://github.com/dominyxu">
+                <i className="fab fa-github"></i>
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.linkedin.com/in/dominyxu/">
+                <i className="fab fa-linkedin"></i>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head';
 import SideBar from '../components/sidebar';
 import Home from './home';
 import homeStyles from '../styles/home.module.css';
-import sidebarStyles from '../components/sidebar.module.css'
+import sidebarStyles from '../components/sidebar.module.css';
+import Script from 'next/script';
 
 
 const siteTitle = 'dominy xu';
@@ -12,6 +13,7 @@ export default function Index() {
     <>
       <Head>
         <title>{siteTitle}</title>
+        <Script src="https://kit.fontawesome.com/c1d5331308.js" crossorigin="anonymous"></Script>
       </Head>
       <div className={homeStyles.mainContainer}>
         <div className={sidebarStyles.sidebarcontainer}>
