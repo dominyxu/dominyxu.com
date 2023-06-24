d02ffb501e4334420fd1631e12165427466d81ff
dominyxu <dominyxu@outlook.com>
2023-06-24 01:43:37 -0400

    moredeits

--- components/sidebar.js
+++ components/sidebar.js
@@ -5,35 +5,31 @@ import Image from 'next/image';
 
 export default function SideBar() {
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/">
-                            <Image
-                                priority
-                                src="/images/h2s_profile.jpeg"
-                                className={sidebarStyles.pfp}
-                                height={250}
-                                width={250}
-                                alt="pfp"
-                            />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/experience">experience</Link>
-                    </li>
-                    <li>
-                        <Link href="/projects">projects</Link>
-                    </li>
+        <nav className={sidebarStyles.nav}>
+            <ul>
+                <li>
+                    <Link href="/">
+                        <Image
+                            priority
+                            src="/images/h2s_profile.jpeg"
+                            className={sidebarStyles.pfp}
+                            height={200}
+                            width={200}
+                            alt="pfp"
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/experience">experience</Link>
+                </li>
+                <li>
+                    <Link href="/projects">projects</Link>
+                </li>
+                <li>
+                    <Link href="/interests">interests</Link>
+                </li>
 
-
-                    <li>
-                        <Link href="/interests">interests</Link>
-                    </li>
-
-                </ul>
-            </nav>
-        </div>
+            </ul>
+        </nav>
     )
 }
