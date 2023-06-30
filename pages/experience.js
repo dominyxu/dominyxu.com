ebfeedda1f01f78d82505ca1317e3c0ea5898273
dominyxu <dominyxu@outlook.com>
2023-06-30 01:53:15 -0400

    satyles

--- pages/experience.js
+++ pages/experience.js
@@ -6,7 +6,17 @@ export default function Experience() {
             <div className="experience">
                 <h1>experience</h1>
                 <br></br>
+                <h2>Ministry of Health</h2>
+                <br></br>
                 <p>hihihihihihihihihihihihihihihihihihihihihihihihihihihi aljdfvnajkfv ohjfdajvkladfvkladf </p>
+                <br></br>
+                    <p>
+                        I am a third year computer engineering student based in Toronto studying @McMasterU. I’m interested in cloud technologies, machine learning and fintech with the ultimate goal of creating better products through software.
+                        <br></br>
+                        <br></br>
+
+                        Feel free to reach out to me at and grab a copy of my resume!
+                    </p>
             </div>
         </>
     );
