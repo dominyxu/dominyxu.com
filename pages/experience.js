dce9655aa4c33d5dcd602af6516febdeb940fabd
dominyxu <dominyxu@outlook.com>
2023-07-03 13:54:48 -0400

    testing style

--- pages/experience.js
+++ pages/experience.js
@@ -17,6 +17,20 @@ export default function Experience() {
 
                         Feel free to reach out to me at and grab a copy of my resume!
                     </p>
+                    <p>
+                        I am a third year computer engineering student based in Toronto studying @McMasterU. I’m interested in cloud technologies, machine learning and fintech with the ultimate goal of creating better products through software.
+                        <br></br>
+                        <br></br>
+
+                        Feel free to reach out to me at and grab a copy of my resume!
+                    </p>
+                    <p>
+                        I am a third year computer engineering student based in Toronto studying @McMasterU. I’m interested in cloud technologies, machine learning and fintech with the ultimate goal of creating better products through software.
+                        <br></br>
+                        <br></br>
+
+                        Feel free to reach out to me at and grab a copy of my resume!
+                    </p>
             </div>
         </>
     );
