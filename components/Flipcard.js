b9e7c573a9bb205fe77b80ddccfac8ec203652de
dominyxu <dominyxu@outlook.com>
2023-07-08 16:54:47 -0400

    flicpard component

---
+++ components/Flipcard.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function Flipcard({pic, role, location, dates, description}){
+
+    const [flip, setFlip] = useState(false);
+
+    return(
+        <div>
+          
+        </div>
+    );
+}
