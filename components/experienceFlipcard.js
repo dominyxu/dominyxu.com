551decbfcadd3b45627ffd888514e86c0912e665
dominyxu <dominyxu@outlook.com>
2023-07-08 20:37:22 -0400

    flipcard

---
+++ components/experienceFlipcard.js
@@ -0,0 +1,14 @@
+export default function ExperienceFlipcard() {
+  return(
+    <div class="container">
+    <div class="card">
+      <div class="front"></div>
+      <div class="back">
+        <h1>Back of Card</h1>
+        <p>Additional info on the back of the card</p>
+      </div>
+    </div>
+  </div>
+  );
+  
+}
