1a575176451b0c66b5583e561b5e9525ba0ee516
dominyxu <dominyxu@outlook.com>
2023-07-09 23:12:51 -0400

    flicpard uipdatestsetss

--- components/experienceFlipcard.js
+++ components/experienceFlipcard.js
@@ -3,8 +3,8 @@ import flipcardStyles from './experienceFlipcard.module.css'
 export default function ExperienceFlipcard() {
   return (
     <div className={flipcardStyles.flipcardContainer}>
-      <div className={flipcardStyles.back}></div>
-      <div className={flipcardStyles.front}></div>
+      <div className={flipcardStyles.back}>backside</div>
+      <div className={flipcardStyles.front}>front</div>
 
 
     </div>
