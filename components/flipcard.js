5691c476b63ec6c2536828cc743aaf438cf90636
dominyxu <dominyxu@outlook.com>
2023-07-12 02:49:05 -0400

    flipcard logic

--- components/flipcard.js
+++ components/flipcard.js
@@ -1,11 +1,32 @@
 import flipcardStyles from './flipcard.module.css'
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function Flipcard( {onClick} ) {
-  return (
-    <div className={flipcardStyles.card} onClick={onClick}>
-      <div className={flipcardStyles.back}>iback</div>
-      <div className={flipcardStyles.front}>ofront</div>
-    </div>
-  );
+export default function Flipcard({ logo, role, location, date, content, onClick }) {
+    return (
+        <div className={flipcardStyles.card} onClick={onClick}>
+            <div className={flipcardStyles.back}>
+                back
+            </div>
+            <div className={flipcardStyles.front}>
+                <div className='left'>
+                <h3><Image
+                        priority
+                        src={logo}
+                        className={flipcardStyles.logo}
+                        height={175}
+                        width={175}
+                        alt="logo"
+                    />
+                    <Link href="https://www.eng.mcmaster.ca/ece/">McMaster University</Link>
+                    Computer Engineering Student</h3>
+                </div>
+                <div className='right'>
+<h3>{date}</h3>
+                </div>
+                
+            </div>
+        </div>
+    );
 
 }
