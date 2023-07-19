bbf65ab565ce09c7e63806dd6345c855641b1af5
dominyxu <dominyxu@outlook.com>
2023-07-19 03:25:39 -0400

    education tab

--- components/experienceFlipcard.js
+++ components/experienceFlipcard.js
@@ -3,13 +3,34 @@ import { CSSTransition } from 'react-transition-group';
 import { useState } from "react";
 import Flipcard from './Flipcard';
 import experienceFlipcardStyles from './experienceFlipcard.module.css'
+import Link from 'next/link';
 
 export default function ExperienceFlipcard() {
 
-  const [showFront, setShowFront] = useState(true); //useState to determine if we show the front or the back of the card
+    const [showFront, setShowFront] = useState(true); //useState to determine if we show the front or the back of the card
 
-  return (
-      <div className={experienceFlipcardStyles.flippableCardContainer}>
+    return (
+        <div className={experienceFlipcardStyles.flippableCardContainer}>
+            <Flipcard
+                logo="/images/mcmaster.png"
+                role="computer engineering studentlol"
+                company="MCMASTER UNIVERSITYlol"
+                companyLink="https://www.eng.mcmaster.ca/ece/"
+                location="Hamilton, ON"
+                date="05/25"
+                content={
+                    <div>
+                        <ul>
+                            <li>Led development on an Electric Logging Device prototype using React and Tailwind CSS to the technical standard
+                                from the CCMTA.</li>
+                            <li>Designed and implemented UI changes to company website.</li>
+                            <li>Analyzed and oversaw technical fleet management data for City of Richmond Hill vehicles.</li>
+
+                        </ul>
+                    </div>
+                }
+            ></Flipcard>
+            <hr></hr>
             <CSSTransition
                 in={showFront}
                 timeout={300}
@@ -19,10 +40,10 @@ export default function ExperienceFlipcard() {
                     setShowFront((v) => !v);
                 }}></Flipcard>
             </CSSTransition>
+
             
-            <Flipcard></Flipcard>
 
         </div>
-  );
+    );
 
 }
--- components/flipcard.js
+++ components/flipcard.js
@@ -2,30 +2,28 @@ import flipcardStyles from './flipcard.module.css'
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function Flipcard({ logo, role, location, date, content, onClick }) {
+export default function Flipcard({ logo, role, company, companyLink, location, date, content, onClick }) {
     return (
         <div className={flipcardStyles.card} onClick={onClick}>
             <div className={flipcardStyles.back}>
-                {/* {content} */}
-                <div>
-                    <>
-                        <ul>
-                            <li>Led development on an Electric Logging Device prototype using React and Tailwind CSS to the technical standard
-                                from the CCMTA.</li>
-                            <li>Designed and implemented UI changes to company website.</li>
-                            <li>Analyzed and oversaw technical fleet management data for City of Richmond Hill vehicles.</li>
+                {content}
+                {/* <div>
+                    <ul>
+                        <li>Led development on an Electric Logging Device prototype using React and Tailwind CSS to the technical standard
+                            from the CCMTA.</li>
+                        <li>Designed and implemented UI changes to company website.</li>
+                        <li>Analyzed and oversaw technical fleet management data for City of Richmond Hill vehicles.</li>
 
-                        </ul>
-                    </>
-                </div>
+                    </ul>
+                </div> */}
 
             </div>
             <div className={flipcardStyles.front}>
                 <h2 className={flipcardStyles.left}>
                     <div>
                         <Image
-                            /*src={logo}*/
-                            src="/images/mcmaster.png"
+                            src={logo}
+                            // src="/images/mcmaster.png"
                             className={flipcardStyles.logo}
                             height={75}
                             width={75}
@@ -34,20 +32,22 @@ export default function Flipcard({ logo, role, location, date, content, onClick 
                     </div>
                     <div className={flipcardStyles.info}>
                         <div>
-                            <Link href="https://www.eng.mcmaster.ca/ece/">MCMASTER UNIVERSITY</Link>
+                            
+                            {/*MCMASTER UNIVERSITY*/}
+                            <Link href={`${companyLink}`}>{company}</Link>
                             <br></br>
                         </div>
-                        {/*{role}*/}
-                        computer engineering student
+                        {role}
+                        {/* computer engineering student */}
                     </div>
                 </h2>
                 <div className={flipcardStyles.right}>
-                    {/*
+                    
                     <p>{location}</p>
-                    <h3>{date}</h3> 
-                    */}
-                    <p>Hamilton, ON</p>
-                    <p>05/25</p>
+                    <p>{date}</p> 
+                    
+                    {/* <p>Hamilton, ON</p>
+                    <p>05/25</p> */}
                 </div>
 
             </div>
